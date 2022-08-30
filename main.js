1bc56b278f622a8e97a8953a9ca42ff5000a0fe1
RoninXYZ <xyzronin97@gmail.com>
2022-08-31 06:56:16 +0700

    lat javascripts

---
+++ main.js
@@ -0,0 +1,23 @@
+const myNumber = 42;
+const myFloat = 42.01;
+const myString = "42.123abcd"
+
+// console.log(myNumber);
+// console.log(myFloat);
+// console.log(myString);
+
+// console.log(myNumber === myFloat);
+// console.log(myString +3);
+// console.log(Number(myString) === myNumber);
+// console.log(Number(true));
+// console.log(Number(false));
+
+// console.log(Number.isInteger(myNumber));
+
+// console.log(Number.isInteger(myFloat));
+//console.log(Number.isInteger(myString));
+// console.log(Number.parseFloat(myFloat));
+// console.log(Number.parseFloat(myNumber));
+//console.log(Number.parseFloat(myString).toFixed(3));
+// console.log(typeof myFloat.toString());
+//console.log(typeof Number.parseFloat(myString))
---
+++ main1.js
@@ -0,0 +1,5 @@
+// console.log(Number("Dave"));
+// console.log(Number.isNaN("Dave"));
+// console.log(isNaN("Dave"));
+//console.log(Math.PI);
+console.log(Math.trunc(Math.PI));

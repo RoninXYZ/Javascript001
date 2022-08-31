139b85ce2503d3ac485d2655b766b735c4d5c115
RoninXYZ <xyzronin97@gmail.com>
2022-08-31 14:44:59 +0700

    random number

---
+++ RanddomNumber/app.js
@@ -0,0 +1,15 @@
+let x //= Math.floor(Math.random() * 6) + 1;
+let y //= Math.floor(Math.random() * 6) + 1;
+let z //= Math.floor(Math.random() * 6) + 1;
+//console.log(x);
+//console.log(y);
+//console.log(z);
+
+document.getElementById("rollbutton").onclick = function () {
+     x = Math.floor(Math.random() * 6) + 1;
+     y = Math.floor(Math.random() * 6) + 1;
+     z = Math.floor(Math.random() * 6) + 1;
+     document.getElementById("xlabel").innerHTML = x;
+     document.getElementById("ylabel").innerHTML = y;
+     document.getElementById("zlabel").innerHTML = z;
+}

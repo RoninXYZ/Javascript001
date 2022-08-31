bbee92bb8e11c2371da4be4d1512ad9703d5f29b
RoninXYZ <xyzronin97@gmail.com>
2022-08-31 12:39:53 +0700

    latihan javascript

--- app.js
+++ app.js
@@ -1,8 +1,37 @@
 // let username = window.prompt("what is your name ?");
 // console.log(username);
 
-document.getElementById("myButton").onclick = function(){
-    username=document.getElementById("myText").value;
-    console.log(username);
-    document.getElementById("myLabel").innerHTML = "Hello " + username;
-}
+// document.getElementById("myButton").onclick = function(){
+//     username=document.getElementById("myText").value;
+//     console.log(username);
+//     document.getElementById("myLabel").innerHTML = "Hello " + username;
+// }
+
+// document.getElementById("myButton").onclick = function(){
+//     console.log("Hello Aliens")
+// }
+
+// let no = window.prompt("no berapa kamu ??");
+// console.log(typeof no);
+// no = Number(no);
+// console.log(typeof no);
+// no += 1;
+// console.log("happy , you are no " + no)
+
+
+// let x;
+// let y;
+// let z;
+// x = Number("3.14");
+// y = String(3.14);
+// z = Boolean("pizza");
+
+
+
+
+// console.log( x , typeof x);
+// console.log(y , typeof y);
+// console.log(z , typeof z);
+
+
+
---
+++ app1.js
@@ -0,0 +1,10 @@
+let pi = 3.14159;
+let radius;
+let circumference;
+radius = window.prompt("Enter the radius of the circle??");
+radius = Number(radius);
+
+pi = 420.69;
+circumference = 2 * pi * radius;
+
+console.log("this circumference is   "+ circumference);

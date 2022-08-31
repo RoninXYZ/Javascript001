aeccc8dc344b2a77e407c2ca798a06dc38f5239e
RoninXYZ <xyzronin97@gmail.com>
2022-08-31 14:59:28 +0700

    string method

---
+++ useStringMethod/app.js
@@ -0,0 +1,10 @@
+let username = "Bro, .. code";
+
+
+let namelength = username.length;
+let chart = username.charAt(0);
+let alfa = username.indexOf("o");
+document.getElementById("leng").innerHTML=namelength;
+document.getElementById("charAt").innerHTML=chart;
+document.getElementById("index").innerHTML=alfa;
+

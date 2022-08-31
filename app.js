950b5e39b55341667612b4893ff7005c0aab9231
RoninXYZ <xyzronin97@gmail.com>
2022-08-31 12:12:37 +0700

    baru

--- app.js
+++ app.js
@@ -4,4 +4,5 @@
 document.getElementById("myButton").onclick = function(){
     username=document.getElementById("myText").value;
     console.log(username);
+    document.getElementById("myLabel").innerHTML = "Hello " + username;
 }

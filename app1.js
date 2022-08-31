59f7945bf4accc0eeb5c5db55e923259d3c0f8b2
RoninXYZ <xyzronin97@gmail.com>
2022-08-31 12:54:40 +0700

    latihan javascript math

--- app1.js
+++ app1.js
@@ -1,10 +1,29 @@
-let pi = 3.14159;
-let radius;
-let circumference;
-radius = window.prompt("Enter the radius of the circle??");
-radius = Number(radius);
+// const pi = 3.14159;
+// let radius;
+// let circumference;
+// radius = window.prompt("Enter the radius of the circle??");
+// radius = Number(radius);
 
-pi = 420.69;
-circumference = 2 * pi * radius;
+// // pi = 420.69;
+// circumference = 2 * pi * radius;
 
-console.log("this circumference is   "+ circumference);
+// console.log("this circumference is   "+ circumference);
+
+// let x = -3.14;
+// x = Math.round(x);
+//x = Math.ceil(x);
+// x = Math.pow(x ,2);
+// x = Math.abs(x);
+let x = -3.14;
+let y = 5;
+let z = 9;
+let maximum ;
+let minimum ;
+
+// maximum= Math.max(x,y,z);
+// minimum = Math.min(x,y,z);
+// console.log(maximum);
+// console.log(minimum);
+
+x = Math.PI;
+console.log(x);

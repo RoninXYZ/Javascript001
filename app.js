5bab71a12dd676383591ac6a3eff125d513430a1
RoninXYZ <xyzronin97@gmail.com>
2022-08-31 12:09:21 +0700

    app.js id

--- app.js
+++ app.js
@@ -1,2 +1,7 @@
-let username = window.prompt("what is your name ?");\
-console.log(username);
+// let username = window.prompt("what is your name ?");
+// console.log(username);
+
+document.getElementById("myButton").onclick = function(){
+    username=document.getElementById("myText").value;
+    console.log(username);
+}

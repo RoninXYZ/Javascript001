603b7e31f04803ce6219deeb7db4a4c9a7cd5427
RoninXYZ <xyzronin97@gmail.com>
2022-08-31 11:59:41 +0700

    app.js tambah

---
+++ app.js
@@ -0,0 +1,2 @@
+let username = window.prompt("what is your name ?");\
+console.log(username);
--- main2.js
+++ main2.js
@@ -4,18 +4,22 @@
 
 // }
 
-let Costumerisbanned = false;
-let soup = "chicken noodle soup";
-let crackes = true;
-let replay;
-if(Costumerisbanned){
-    replay = `no soup for you`
-}else if(soup && crackes){
-    replay= `Here is your order ${soup} & crackes`;
-}else if(soup){
-    replay= `Here is your order ${soup}`;
-}
-else{
-    replay=`sory we are out of soup`;
-}
-console.log(replay);
+// let Costumerisbanned = false;
+// let soup = "chicken noodle soup";
+// let crackes = true;
+// let replay;
+// if(Costumerisbanned){
+//     replay = `no soup for you`
+// }else if(soup && crackes){
+//     replay= `Here is your order ${soup} & crackes`;
+// }else if(soup){
+//     replay= `Here is your order ${soup}`;
+// }
+// else{
+//     replay=`sory we are out of soup`;
+// }
+// console.log(replay);
+
+
+let testScore = 98;
+let grade;

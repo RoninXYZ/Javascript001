8efdc9ad6349e1aaece5809d986d50705824ac46
RoninXYZ <xyzronin97@gmail.com>
2022-09-01 06:50:20 +0700

    Latihan javascript property

---
+++ property/app.js
@@ -0,0 +1,7 @@
+document.getElementById("myIdButton").onclick = function(){
+    if(document.getElementById("myCheckbox").checked ){
+     console.log("you are subscribed")
+    }else{
+        console.log("you are Not subscribed")
+    }
+}

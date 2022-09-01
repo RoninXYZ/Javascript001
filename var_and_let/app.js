fe17a364a136976ea7acc1a5bab7a61d9e5b8b70
RoninXYZ <xyzronin97@gmail.com>
2022-09-01 10:48:55 +0700

    Latihan javascript var and let

---
+++ var_and_let/app.js
@@ -0,0 +1,17 @@
+// var limited to the function(){}
+//let = variable are limited to the block scope
+
+// for(var i = 0 ; i < 10 ; i++){
+//     //console.log(i);
+// }
+// console.log(i);
+
+do_somtghing();
+function do_somtghing(){
+    for(var i = 0 ; i <5 ; i++){
+        //console.log(i)
+    }
+
+}
+
+console.log(i);

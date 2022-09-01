a7795b7f3c8a2633c331f117e29eda78e48db738
RoninXYZ <xyzronin97@gmail.com>
2022-09-01 13:46:06 +0700

    function

---
+++ function1/app.js
@@ -0,0 +1,56 @@
+// function add(n1,n2){
+//     var sum = n1 + n2;
+//     console.log( "hasilnya adalah " + sum);
+// }
+
+// add(12,13);
+
+// function add(a, b = 1) {
+//     return a+b;
+//     }
+// console.log(add(4))
+
+// function add( a, b=1){
+//     return a + b;
+// }
+
+// console.log(add(4,3));
+
+
+// var res = function(){return "Hello"}
+// console.log(res())
+
+// var func = function (x, y) { return x * y };
+// function product() {
+//     var result;
+//     result = func(10, 20);
+//     console.log("The product : " + result)
+// }
+// product()
+
+
+// var func = function(x,y) {return x* y}
+
+// function product (){
+//     var result;
+//     result = func(10,20);
+//     console.log("the product :" +result)
+// }
+
+// product()
+
+// var func = new Function("x", "y", "return x*y;");
+// function product() {
+//     var result;
+
+
+
+//     result = func(10, 20);
+//     console.log("The product : " + result)
+// }
+// product()
+
+
+setTimeout(()=>{
+    console.log("Learning is fun");
+},1000)

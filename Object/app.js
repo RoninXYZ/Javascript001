eecc019ecd8c9c9a8086e072ba4042f3f2443011
RoninXYZ <xyzronin97@gmail.com>
2022-09-01 16:46:15 +0700

    Object

---
+++ Object/app.js
@@ -0,0 +1,16 @@
+// var person ={
+//     firstName :"Tom",
+//     lastName :"Hanks",
+//     func:function(){
+//         return "Hello"
+//     }
+// }
+
+// console.log(person.firstName);
+// console.log(person.func())
+
+// var foo ="barr";
+// var baz = {foo};
+// console.log(baz.foo)
+
+
